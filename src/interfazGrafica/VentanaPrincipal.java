7e8b1dc3807679189b0b0d58049b174b7e5cb468
imelinc <ignaciomelinc@gmail.com>
2025-05-28 22:00:39 -0300

    Agregue la pantalla principal

--- src/interfazGrafica/VentanaPrincipal.java
+++ src/interfazGrafica/VentanaPrincipal.java
@@ -5,6 +5,8 @@ import elementos.GestorEventos;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -46,7 +48,44 @@ public class VentanaPrincipal extends JFrame {
 
         // acciones de los botones
         agregarBoton.addActionListener(e -> {
-            Formulario formulario = new Formulario();
-        })
+            Formulario formulario = new Formulario(this, "Agregar Eventos", null);
+            Evento nuevo = formulario.getEvento();
+            if (nuevo != null) {
+                gestor.agregarEvento(nuevo);
+                listModel.addElement(nuevo);
+            }
+        });
+
+        editarBoton.addActionListener(e -> {
+           Evento nuevo = listaEventos.getSelectedValue();
+           if (nuevo != null) {
+               int index = listaEventos.getSelectedIndex();
+               Formulario formulario = new Formulario(this, "Editar Eventos", nuevo);
+               Evento eventoModif = formulario.getEvento();
+               if (eventoModif != null) {
+                   gestor.actualizarEvento(index,eventoModif);
+                   listModel.set(index, eventoModif);
+               }
+           } else {
+               JOptionPane.showMessageDialog(this, "Seleccione un evento para editar");
+           }
+        });
+
+        detalleBoton.addActionListener(e -> {
+            Evento nuevo = listaEventos.getSelectedValue();
+            if (nuevo != null) {
+                new DetalleEvento(this, nuevo);
+            } else {
+                JOptionPane.showMessageDialog(this, "Seleccione un evento para ver los detalles");
+            }
+        });
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                gestor.guardarEnArchivo("eventos.txt");
+                dispose();
+            }
+        });
+        setVisible(true);
     }
 }
